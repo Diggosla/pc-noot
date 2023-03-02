ac1c2893fa1c71b6279be837c445ecea99bf0901
Diogo Zanetti <diogozanetti28@gmail.com>
2023-03-01 22:18:51 -0300

    Adicionar arquivos de projeto.

---
+++ pc-noot/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace pc_noot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxmostrar1pc = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmostrar2pc = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmostrar3pc = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmostrar1not = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmostrar2not = new System.Windows.Forms.PictureBox();
+            this.pictureBoxmostrar3not = new System.Windows.Forms.PictureBox();
+            this.buttonpc = new System.Windows.Forms.RadioButton();
+            this.button2pc = new System.Windows.Forms.RadioButton();
+            this.button3pc = new System.Windows.Forms.RadioButton();
+            this.button4pc = new System.Windows.Forms.RadioButton();
+            this.buttonnot = new System.Windows.Forms.RadioButton();
+            this.button2not = new System.Windows.Forms.RadioButton();
+            this.button3not = new System.Windows.Forms.RadioButton();
+            this.button4not = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar1pc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar2pc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar3pc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar1not)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar2not)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar3not)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxmostrar1pc
+            // 
+            this.pictureBoxmostrar1pc.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar1pc.Image")));
+            this.pictureBoxmostrar1pc.Location = new System.Drawing.Point(65, 53);
+            this.pictureBoxmostrar1pc.Name = "pictureBoxmostrar1pc";
+            this.pictureBoxmostrar1pc.Size = new System.Drawing.Size(100, 95);
+            this.pictureBoxmostrar1pc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar1pc.TabIndex = 0;
+            this.pictureBoxmostrar1pc.TabStop = false;
+            this.pictureBoxmostrar1pc.Visible = false;
+            // 
+            // pictureBoxmostrar2pc
+            // 
+            this.pictureBoxmostrar2pc.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar2pc.Image")));
+            this.pictureBoxmostrar2pc.Location = new System.Drawing.Point(65, 166);
+            this.pictureBoxmostrar2pc.Name = "pictureBoxmostrar2pc";
+            this.pictureBoxmostrar2pc.Size = new System.Drawing.Size(100, 95);
+            this.pictureBoxmostrar2pc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar2pc.TabIndex = 1;
+            this.pictureBoxmostrar2pc.TabStop = false;
+            this.pictureBoxmostrar2pc.Visible = false;
+            // 
+            // pictureBoxmostrar3pc
+            // 
+            this.pictureBoxmostrar3pc.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar3pc.Image")));
+            this.pictureBoxmostrar3pc.Location = new System.Drawing.Point(63, 290);
+            this.pictureBoxmostrar3pc.Name = "pictureBoxmostrar3pc";
+            this.pictureBoxmostrar3pc.Size = new System.Drawing.Size(102, 95);
+            this.pictureBoxmostrar3pc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar3pc.TabIndex = 2;
+            this.pictureBoxmostrar3pc.TabStop = false;
+            this.pictureBoxmostrar3pc.Visible = false;
+            // 
+            // pictureBoxmostrar1not
+            // 
+            this.pictureBoxmostrar1not.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar1not.Image")));
+            this.pictureBoxmostrar1not.Location = new System.Drawing.Point(412, 53);
+            this.pictureBoxmostrar1not.Name = "pictureBoxmostrar1not";
+            this.pictureBoxmostrar1not.Size = new System.Drawing.Size(102, 95);
+            this.pictureBoxmostrar1not.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar1not.TabIndex = 2;
+            this.pictureBoxmostrar1not.TabStop = false;
+            this.pictureBoxmostrar1not.Visible = false;
+            // 
+            // pictureBoxmostrar2not
+            // 
+            this.pictureBoxmostrar2not.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar2not.Image")));
+            this.pictureBoxmostrar2not.Location = new System.Drawing.Point(412, 166);
+            this.pictureBoxmostrar2not.Name = "pictureBoxmostrar2not";
+            this.pictureBoxmostrar2not.Size = new System.Drawing.Size(102, 95);
+            this.pictureBoxmostrar2not.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar2not.TabIndex = 2;
+            this.pictureBoxmostrar2not.TabStop = false;
+            this.pictureBoxmostrar2not.Visible = false;
+            // 
+            // pictureBoxmostrar3not
+            // 
+            this.pictureBoxmostrar3not.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxmostrar3not.Image")));
+            this.pictureBoxmostrar3not.Location = new System.Drawing.Point(412, 290);
+            this.pictureBoxmostrar3not.Name = "pictureBoxmostrar3not";
+            this.pictureBoxmostrar3not.Size = new System.Drawing.Size(102, 95);
+            this.pictureBoxmostrar3not.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxmostrar3not.TabIndex = 2;
+            this.pictureBoxmostrar3not.TabStop = false;
+            this.pictureBoxmostrar3not.Visible = false;
+            // 
+            // buttonpc
+            // 
+            this.buttonpc.AutoSize = true;
+            this.buttonpc.Location = new System.Drawing.Point(65, 21);
+            this.buttonpc.Name = "buttonpc";
+            this.buttonpc.Size = new System.Drawing.Size(85, 17);
+            this.buttonpc.TabIndex = 3;
+            this.buttonpc.TabStop = true;
+            this.buttonpc.Text = "radioButton1";
+            this.buttonpc.UseVisualStyleBackColor = true;
+            this.buttonpc.CheckedChanged += new System.EventHandler(this.buttonpc_CheckedChanged);
+            // 
+            // button2pc
+            // 
+            this.button2pc.AutoSize = true;
+            this.button2pc.Location = new System.Drawing.Point(171, 96);
+            this.button2pc.Name = "button2pc";
+            this.button2pc.Size = new System.Drawing.Size(85, 17);
+            this.button2pc.TabIndex = 3;
+            this.button2pc.TabStop = true;
+            this.button2pc.Text = "radioButton1";
+            this.button2pc.UseVisualStyleBackColor = true;
+            this.button2pc.CheckedChanged += new System.EventHandler(this.button2pc_CheckedChanged);
+            // 
+            // button3pc
+            // 
+            this.button3pc.AutoSize = true;
+            this.button3pc.Location = new System.Drawing.Point(171, 205);
+            this.button3pc.Name = "button3pc";
+            this.button3pc.Size = new System.Drawing.Size(85, 17);
+            this.button3pc.TabIndex = 3;
+            this.button3pc.TabStop = true;
+            this.button3pc.Text = "radioButton1";
+            this.button3pc.UseVisualStyleBackColor = true;
+            this.button3pc.CheckedChanged += new System.EventHandler(this.button3pc_CheckedChanged);
+            // 
+            // button4pc
+            // 
+            this.button4pc.AutoSize = true;
+            this.button4pc.Location = new System.Drawing.Point(171, 339);
+            this.button4pc.Name = "button4pc";
+            this.button4pc.Size = new System.Drawing.Size(85, 17);
+            this.button4pc.TabIndex = 3;
+            this.button4pc.TabStop = true;
+            this.button4pc.Text = "radioButton1";
+            this.button4pc.UseVisualStyleBackColor = true;
+            this.button4pc.CheckedChanged += new System.EventHandler(this.button4pc_CheckedChanged);
+            // 
+            // buttonnot
+            // 
+            this.buttonnot.AutoSize = true;
+            this.buttonnot.Location = new System.Drawing.Point(412, 21);
+            this.buttonnot.Name = "buttonnot";
+            this.buttonnot.Size = new System.Drawing.Size(85, 17);
+            this.buttonnot.TabIndex = 3;
+            this.buttonnot.TabStop = true;
+            this.buttonnot.Text = "radioButton1";
+            this.buttonnot.UseVisualStyleBackColor = true;
+            this.buttonnot.CheckedChanged += new System.EventHandler(this.buttonnot_CheckedChanged);
+            // 
+            // button2not
+            // 
+            this.button2not.AutoSize = true;
+            this.button2not.Location = new System.Drawing.Point(520, 96);
+            this.button2not.Name = "button2not";
+            this.button2not.Size = new System.Drawing.Size(85, 17);
+            this.button2not.TabIndex = 3;
+            this.button2not.TabStop = true;
+            this.button2not.Text = "radioButton1";
+            this.button2not.UseVisualStyleBackColor = true;
+            this.button2not.CheckedChanged += new System.EventHandler(this.button2not_CheckedChanged);
+            // 
+            // button3not
+            // 
+            this.button3not.AutoSize = true;
+            this.button3not.Location = new System.Drawing.Point(520, 205);
+            this.button3not.Name = "button3not";
+            this.button3not.Size = new System.Drawing.Size(85, 17);
+            this.button3not.TabIndex = 3;
+            this.button3not.TabStop = true;
+            this.button3not.Text = "radioButton1";
+            this.button3not.UseVisualStyleBackColor = true;
+            this.button3not.CheckedChanged += new System.EventHandler(this.button3not_CheckedChanged);
+            // 
+            // button4not
+            // 
+            this.button4not.AutoSize = true;
+            this.button4not.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4not.Location = new System.Drawing.Point(520, 339);
+            this.button4not.Name = "button4not";
+            this.button4not.Size = new System.Drawing.Size(85, 17);
+            this.button4not.TabIndex = 3;
+            this.button4not.TabStop = true;
+            this.button4not.Text = "radioButton1";
+            this.button4not.UseVisualStyleBackColor = true;
+            this.button4not.CheckedChanged += new System.EventHandler(this.button4not_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 450);
+            this.Controls.Add(this.button4not);
+            this.Controls.Add(this.button3not);
+            this.Controls.Add(this.button2not);
+            this.Controls.Add(this.buttonnot);
+            this.Controls.Add(this.button4pc);
+            this.Controls.Add(this.button3pc);
+            this.Controls.Add(this.button2pc);
+            this.Controls.Add(this.buttonpc);
+            this.Controls.Add(this.pictureBoxmostrar3not);
+            this.Controls.Add(this.pictureBoxmostrar2not);
+            this.Controls.Add(this.pictureBoxmostrar1not);
+            this.Controls.Add(this.pictureBoxmostrar3pc);
+            this.Controls.Add(this.pictureBoxmostrar2pc);
+            this.Controls.Add(this.pictureBoxmostrar1pc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar1pc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar2pc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar3pc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar1not)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar2not)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxmostrar3not)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxmostrar1pc;
+        private System.Windows.Forms.PictureBox pictureBoxmostrar2pc;
+        private System.Windows.Forms.PictureBox pictureBoxmostrar3pc;
+        private System.Windows.Forms.PictureBox pictureBoxmostrar1not;
+        private System.Windows.Forms.PictureBox pictureBoxmostrar2not;
+        private System.Windows.Forms.PictureBox pictureBoxmostrar3not;
+        private System.Windows.Forms.RadioButton buttonpc;
+        private System.Windows.Forms.RadioButton button2pc;
+        private System.Windows.Forms.RadioButton button3pc;
+        private System.Windows.Forms.RadioButton button4pc;
+        private System.Windows.Forms.RadioButton buttonnot;
+        private System.Windows.Forms.RadioButton button2not;
+        private System.Windows.Forms.RadioButton button3not;
+        private System.Windows.Forms.RadioButton button4not;
+    }
+}
+
---
+++ pc-noot/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pc_noot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonpc_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1pc.Visible = true;
+            this.pictureBoxmostrar2pc.Visible = true;
+            this.pictureBoxmostrar3pc.Visible =true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2pc_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1pc.Visible = true;
+            this.pictureBoxmostrar2pc.Visible = false;
+            this.pictureBoxmostrar3pc.Visible = false;
+        }
+
+        private void button3pc_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1pc.Visible = false;
+            this.pictureBoxmostrar2pc.Visible = true;
+            this.pictureBoxmostrar3pc.Visible = false;
+        }
+
+        private void button4pc_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1pc.Visible = false;
+            this.pictureBoxmostrar2pc.Visible = false;
+            this.pictureBoxmostrar3pc.Visible = true;
+        }
+
+        private void buttonnot_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1not.Visible = true;
+            this.pictureBoxmostrar2not.Visible = true;
+            this.pictureBoxmostrar3not.Visible = true;
+        }
+
+        private void button2not_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1not.Visible = true;
+            this.pictureBoxmostrar2not.Visible = false;
+            this.pictureBoxmostrar3not.Visible = false;
+        }
+
+        private void button3not_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1not.Visible = false;
+            this.pictureBoxmostrar2not.Visible = true;
+            this.pictureBoxmostrar3not.Visible = false;
+        }
+
+        private void button4not_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureBoxmostrar1not.Visible = false;
+            this.pictureBoxmostrar2not.Visible = false;
+            this.pictureBoxmostrar3not.Visible = true;
+        }
+    }
+}
